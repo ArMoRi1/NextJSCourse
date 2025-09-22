0c40fe23658678b1602bb4d7f98fdb85bf6e9215
Artem <artemmochalov445@icloud.com>
2025-09-22 16:38:20 -0400

    chapter 16 is done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard', // шаблон для всіх сторінок
+    default: 'Acme Dashboard',       // заголовок за замовчуванням
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://your-domain.com'), // свій домен
+};
+
+
 export default function RootLayout({
   children,
 }: {
